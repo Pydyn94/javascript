62e19ce0cbe13beaae05c537c7390d499bc04010
Grzegorz Pydyn <g.pydyn@gmail.com>
2018-04-22 12:01:30 +0200

    typyDanych1

---
+++ 5-typyDanych/js/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+//typ prosty
+//typ liczbowy
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowita;
+
+calkowita = wyplata + premia;
+
+console.log(calkowita);
+
+
+//lancuch znakow - string
+
+var wyplata = "5000";
+var premia = "200";
+
+var calkowitaa;
+
+calkowitaa = wyplata + premia;
+console.log(calkowitaa);
+
+
+var czyJestSmog;
+czyJestSmog = false;
+
+if (czyJestSmog) {
+    console.log("Jest Smog");
+} else {
+    console.log("Nie ma !");
+}
+
+
+//typy specjalne
+
+var niezdefiniowazmienna;
+var nullowa = null;
+
+console.log(niezdefiniowazmienna);
+console.log(nullowa);
