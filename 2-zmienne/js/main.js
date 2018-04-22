098821dfed4cbc96debf3b43cacc2e1353f24c76
Grzegorz Pydyn <g.pydyn@gmail.com>
2018-04-22 10:24:50 +0200

    zmienne-vol.1

---
+++ 2-zmienne/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+var kolor = "czerwony";
+
+imie = "Sergio";
+
+console.log(imie);
+console.log(imieDamskie);
+console.log(wiek);
+
+//SERGIO został nadpisany przez KAJA
+imie = imieDamskie;
+console.log(imie);
