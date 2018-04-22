837e3aa593ab0ef1e961df77e0f9cb8b32f4088e
Grzegorz Pydyn <g.pydyn@gmail.com>
2018-04-22 16:45:38 +0200

    warsztat-iloczyn-1

---
+++ 13-wasztat-iloczyn/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function dodawanieParametrow(param1, param2, param3) {
+    var wynik = param1 + param2 + param3;
+    return wynik;
+}
+
+var wynikMyFunction = dodawanieParametrow(10, 20, 30);
+console.log(wynikMyFunction);
