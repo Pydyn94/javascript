3b4fbd1aafb7519ec712967711946ced484e4de1
Grzegorz Pydyn <g.pydyn@gmail.com>
2018-04-22 11:26:54 +0200

    funkcje-1

---
+++ 3-funkcje/js/main.js
@@ -0,0 +1,44 @@
+'use strict';
+
+function aktualnyRok() {
+    console.log("2018");
+}
+
+//wywoływanie funkcji
+aktualnyRok();
+aktualnyRok();
+aktualnyRok();
+aktualnyRok();
+
+
+//funkcja anonimowa
+
+var actuallyMonth = function () {
+    console.log("April");
+}
+
+console.log(actuallyMonth);
+
+actuallyMonth();
+actuallyMonth();
+
+
+//funkcja z parametrami
+function dodajLiczby(liczba1, liczba2) {
+    var wynik = liczba1 + liczba2;
+
+    console.log(wynik);
+}
+
+dodajLiczby(4, 7);
+
+
+//funkcje zwracające wartość
+function dodajLiczby(liczba1, liczba2) {
+    var wynik = liczba1 + liczba2;
+
+    return wynik;
+}
+
+var wynikzFunkcji = dodajLiczby(4, 7);
+console.log(wynikzFunkcji);
