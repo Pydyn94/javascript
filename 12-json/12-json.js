42402d1d056ba9ca7a78bb9b4ec0056a95a11aba
Grzegorz Pydyn <g.pydyn@gmail.com>
2018-04-22 16:30:42 +0200

    json-1

--- 12-json/12-json.js
+++ 12-json/12-json.js
@@ -3,14 +3,18 @@
 var jsonOsoby = {
     "osoby": [
         {
-            imie: "Krystian", 
-            nazwisko: "Dziopa", 
-            wzrost: 180, 
-            oczy: "niebieskie", 
-            zainteresowania: [ 
-                { nazwa: "podroze" }, 
-                { nazwa: "gotowanie" }
-            ] 
+            imie: "Krystian",
+            nazwisko: "Dziopa",
+            wzrost: 180,
+            oczy: "niebieskie",
+            zainteresowania: [
+                {
+                    nazwa: "podroze"
+                },
+                {
+                    nazwa: "gotowanie"
+                }
+            ]
         },
         {
             imie: "Dominik",
@@ -18,8 +22,12 @@ var jsonOsoby = {
             wzrost: 170,
             oczy: "brązowe",
             zainteresowania: [
-                { nazwa: "gotowanie" },
-                { nazwa: "języki" }
+                {
+                    nazwa: "gotowanie"
+                },
+                {
+                    nazwa: "języki"
+                }
             ]
         },
         {
@@ -28,22 +36,39 @@ var jsonOsoby = {
             wzrost: 167,
             oczy: "brązowe",
             zainteresowania: [
-                { nazwa: "programowanie" },
-                { nazwa: "podroze" },
-                { nazwa: "języki" }
+                {
+                    nazwa: "programowanie"
+                },
+                {
+                    nazwa: "podroze"
+                },
+                {
+                    nazwa: "języki"
+                }
             ]
-            
+
         }
     ]
 }
 
 console.log(jsonOsoby);
+console.log(jsonOsoby.osoby[0].zainteresowania);
 
+var osoby = jsonOsoby.osoby
 
-jsonOsoby.osoby[2].zainteresowania.forEach( function (element, index) {
-    console.log(element.nazwa);
+osoby.forEach(function (osoba, i) {
+    console.log("Osoba: " + osoba.imie);
+
+    var zainteresowania = osoba.zainteresowania;
+    zainteresowania.forEach(function (zainteresowania, i) {
+        console.log("Zainteresowanie " + zainteresowania.nazwa)
+    })
 });
 
+
+//jsonOsoby.osoby[2].zainteresowania.forEach( function (element, index) {
+//    console.log(element.nazwa);
+//});
 //console.log(jsonOsoby.osoby[2].zainteresowania);
 
 
@@ -51,7 +76,6 @@ jsonOsoby.osoby[2].zainteresowania.forEach( function (element, index) {
 
 
 
-
-//jsonOsoby.osoby[0].zainteresowania.forEach( function(zainteresowanie, index) {
+//jsonOsoby.osoby[0].zainteresowania.forEach(function (zainteresowania, index) {
 //    console.log("index: " + index + " zainteresowanie: " + zainteresowanie.nazwa)
 //});
